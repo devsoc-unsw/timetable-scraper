7be6260b0173f971413d09f6ee9942095391cd56
keshiga <keshigagopalarajah@gamil.com>
2021-07-04 17:05:51 +1000

    updated server.ts getFreeroomsData so that it produces correct object

--- src/server.ts
+++ src/server.ts
@@ -61,92 +61,86 @@ const getCourseList = (req: express.Request, res: express.Response) => {
 //   }
 
 const getFreeroomsData = (req: express.Request, res: express.Response) => {
-	// year is set at 2020 for now
-
   let freeroomsData = {};
-  // NOTE MUST CHANGE DATA TO BE AN ACTUAL OBJECT FOR THIS TO WORK
-  let data = {}
   for (let course of data["T1"]) {
     let courseCode = course["courseCode"];
     let courseName = course["name"];
     for (let classData of course["classes"]) {
-    if (classData["mode"] != "In Person") {
-      continue;
-    }
-  
-    for (let timeElement of classData["times"]) {
-      let [roomName, locationId] = timeElement["location"].split(" (");
-      let [campus, buildingId, roomId] = locationId.split("-");
-  
-      if (!roomId) {
-      continue;
+      if (classData["mode"] != "In Person") {
+        continue;
       }
-  
-      buildingId = campus + "-" + buildingId;
-      roomId = roomId.slice(0, -1);
-  
-      let day = timeElement["day"];
-      let start = timeElement["time"]["start"];
-      let end = timeElement["time"]["end"];
-      let weeks = timeElement["weeks"];
-  
-      // case 1: "weeks": "11"
-      // case 2: "weeks": "1-11",
-      // case 3: "weeks": "3, 5, 7"
-      // case 4: "weeks": "1-2, 3-5, 7-10"
-      // case 5:"weeks": "1-2, 4, 5-7"
-  
-      let allWeeks = weeks.split(", ");
-  
-      for (let tuple in allWeeks) {
-      if (tuple.includes("-")) {
-        // case 1: tuple is a range eg. 1-2
-        let [startRange, endRange] = weeks.split["-"];
-        // turn string into a decimal number after splitting
-        startRange = parseInt(startRange);
-        endRange = parseInt(endRange);
-        for (
-        let currentWeek = startRange;
-        currentWeek <= endRange;
-        currentWeek++
-        ) {
-        inputData(
-          freeroomsData,
-          buildingId,
-          roomId,
-          roomName,
-          currentWeek,
-          day,
-          start,
-          end,
-          courseCode,
-          courseName
-        );
+
+      for (let timeElement of classData["times"]) {
+        let [roomName, locationId] = timeElement["location"].split(" (");
+        let [campus, buildingId, roomId] = locationId.split("-");
+
+        if (!roomId) {
+          continue;
         }
-      } else {
-        // case 2: tuple is an integer eg. 5
-        let currentWeek = parseInt(tuple);
-        inputData(
-        freeroomsData,
-        buildingId,
-        roomId,
-        roomName,
-        currentWeek,
-        day,
-        start,
-        end,
-        courseCode,
-        courseName
-        );
-      }
+
+        buildingId = campus + "-" + buildingId;
+        roomId = roomId.slice(0, -1);
+
+        let day = timeElement["day"];
+        let start = timeElement["time"]["start"];
+        let end = timeElement["time"]["end"];
+        let weeks = timeElement["weeks"];  
+        // case 1: "weeks": "11"
+        // case 2: "weeks": "1-11",
+        // case 3: "weeks": "3, 5, 7"
+        // case 4: "weeks": "1-2, 3-5, 7-10"
+        // case 5:"weeks": "1-2, 4, 5-7"
+        let allWeeks = weeks.split(",");
+
+        for (let tuple in allWeeks) {
+          if (allWeeks[tuple].includes("-")) {
+            // case 1: tuple is a range eg. 1-2
+            let [startRange, endRange] = allWeeks[tuple].split("-");
+            // turn string into a decimal number after splitting
+            let startR = parseInt(startRange);
+            let endR = parseInt(endRange);
+            for (
+              let currentWeek = startR;
+              currentWeek <= endR;
+              currentWeek++
+            ) {
+              inputData(
+                freeroomsData,
+                buildingId,
+                roomId,
+                roomName,
+                currentWeek,
+                day,
+                start,
+                end,
+                courseCode,
+                courseName
+              );
+            }
+          } else {
+            // case 2: tuple is an integer eg. 5
+            let currentWeek = parseInt(allWeeks[tuple]);
+            inputData(
+              freeroomsData,
+              buildingId,
+              roomId,
+              roomName,
+              currentWeek,
+              day,
+              start,
+              end,
+              courseCode,
+              courseName
+            );
+          }
+        }
       }
     }
-    }
-  }
-  console.log(freeroomsData["K-E12"]["114"]);
+  } 
+
+  console.log(freeroomsData["K-E12"]["114"]["3"]);
 }
-	  
-	  
+  
 function inputData(
   freeroomsData,
   buildingId,
@@ -158,7 +152,7 @@ function inputData(
   end,
   courseCode,
   courseName
-  ) {
+) {
   if (!(buildingId in freeroomsData)) {
     freeroomsData[buildingId] = {};
   }
@@ -179,17 +173,14 @@ function inputData(
     start: start,
     end: end,
   });
- }
-	  
-const getAllData = (req: express.Request, res: express.Response) => {
-	res.send(timetableData);
 }
+  
 
 
 app.get('/api/terms/:termId/courses/:courseId', getCourse);
 app.get('/api/terms/:termId/courses', getCourseList);
 app.get('/api/freerooms', getFreeroomsData);
-app.get('/api/', getAllData);
+//app.get('/api/', getAllData);
 
 app.use((_, res, next) => {
 	// update to match the domain you will make the request from
