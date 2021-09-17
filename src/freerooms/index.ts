7b29cd9a7eb887502b8f50ad617f2d35ef8d998a
Mun Joon Teo <teomunjoon101@gmail.com>
2021-09-17 21:13:58 +1000

    Fixed some small bugs and improved style

--- src/freerooms/index.ts
+++ src/freerooms/index.ts
@@ -1,72 +1,65 @@
 import * as express from "express";
+import { Course } from "../interfaces";
 import { timetableData } from "../load-data";
 
-// Running: npx nodemon --exec npm start
-
-// returns
-// {
-// 	  "BuildingID": {
-// 		"RoomID": {
-// 		  "name": "RoomName",
-// 		  "Week": {
-// 			"Day": [
-// 			  {
-// 				"courseCode": "courseCodeText",
-// 				"start": "Time_Text",
-// 				"end": "Time_Text"
-// 			  }
-// 			]
-// 		  }
-// 		}
-// 	  }
-// 	}
-//   }
-
 const getFreeroomsData = (req: express.Request, res: express.Response) => {
+  const term = req.params.termId.substring(5);
+  const termData: Course[] = timetableData[term];
+
   let freeroomsData = {};
 
-  if (!timetableData["T1"]) {
-    res.send(freeroomsData);
-  }
-
-  for (let course of timetableData["T1"]) {
+  for (let course of termData) {
     let courseCode = course["courseCode"];
     let courseName = course["name"];
-    for (let classData of course["classes"]) {
-      if (classData["mode"] != "In Person") {
-        continue;
-      }
+
+    let courseClasses = course["classes"];
+    if (!courseClasses) continue;
+
+    for (let classData of courseClasses) {
+      if (classData["mode"] !== "In Person") continue;
 
       for (let timeElement of classData["times"]) {
-        let [roomName, locationId] = timeElement["location"].split(" (");
-        let [campus, buildingId, roomId] = locationId.split("-");
+        let classLocation = timeElement["location"];
+        if (!classLocation) continue;
 
-        if (!roomId) {
-          continue;
-        }
+        // An example location is UNSW Business School 219 (K-E12-219)
+        let [roomName, locationId] = classLocation.split(" (");
+        let [campus, buildingId, roomId] = locationId.split("-");
+        if (!roomId) continue;
 
         buildingId = campus + "-" + buildingId;
+
+        // Remove trailing ) from room id
         roomId = roomId.slice(0, -1);
 
         let day = timeElement["day"];
-        let start = timeElement["time"]["start"];
-        let end = timeElement["time"]["end"];
+        let startTime = timeElement["time"]["start"];
+        let endTime = timeElement["time"]["end"];
         let weeks = timeElement["weeks"];
-        // case 1: "weeks": "11"
-        // case 2: "weeks": "1-11",
-        // case 3: "weeks": "3, 5, 7"
-        // case 4: "weeks": "1-2, 3-5, 7-10"
-        // case 5:"weeks": "1-2, 4, 5-7"
+
+        // case 1: "weeks": "11" (single week)
+        // case 2: "weeks": "1-11" (one range of weeks)
+        // case 3: "weeks": "3, 5, 7" (list of individual weeks)
+        // case 4: "weeks": "1-2, 3-5, 7-10" (list of range of weeks)
+        // case 5: "weeks": "1-2, 4, 5-7" (list of both individual and range of weeks)
+
         let allWeeks = weeks.split(",");
 
-        for (let tuple in allWeeks) {
-          if (allWeeks[tuple].includes("-")) {
-            // case 1: tuple is a range eg. 1-2
-            let [startRange, endRange] = allWeeks[tuple].split("-");
+        for (let week in allWeeks) {
+          if (allWeeks[week].includes("-")) {
+            // case 1: week is a range eg. 1-2
+            let [startRange, endRange] = allWeeks[week].split("-");
+
             // turn string into a decimal number after splitting
-            let startR = parseInt(startRange);
-            let endR = parseInt(endRange);
-            for (let currentWeek = startR; currentWeek <= endR; currentWeek++) {
+            // it will be converted back into a string when used as a key in the object
+            let startWeek = parseInt(startRange);
+            let endWeek = parseInt(endRange);
+
+            for (
+              let currentWeek = startWeek;
+              currentWeek <= endWeek;
+              currentWeek++
+            ) {
               inputData(
                 freeroomsData,
                 buildingId,
@@ -74,15 +67,17 @@ const getFreeroomsData = (req: express.Request, res: express.Response) => {
                 roomName,
                 currentWeek,
                 day,
-                start,
-                end,
+                startTime,
+                endTime,
                 courseCode,
                 courseName
               );
             }
           } else {
-            // case 2: tuple is an integer eg. 5
-            let currentWeek = parseInt(allWeeks[tuple]);
+            // case 2: week is an integer eg. 5
+            // turn string into a decimal number for consistency with case 1
+            let currentWeek = parseInt(allWeeks[week]);
+
             inputData(
               freeroomsData,
               buildingId,
@@ -90,8 +85,8 @@ const getFreeroomsData = (req: express.Request, res: express.Response) => {
               roomName,
               currentWeek,
               day,
-              start,
-              end,
+              startTime,
+              endTime,
               courseCode,
               courseName
             );
@@ -105,36 +100,41 @@ const getFreeroomsData = (req: express.Request, res: express.Response) => {
 };
 
 function inputData(
-  freeroomsData,
-  buildingId,
-  roomId,
-  roomName,
-  currentWeek,
-  day,
-  start,
-  end,
-  courseCode,
-  courseName
+  freeroomsData: {},
+  buildingId: string,
+  roomId: string,
+  roomName: string,
+  currentWeek: number,
+  day: string,
+  startTime: string,
+  endTime: string,
+  courseCode: string,
+  courseName: string
 ) {
   if (!(buildingId in freeroomsData)) {
     freeroomsData[buildingId] = {};
   }
+
   if (!(roomId in freeroomsData[buildingId])) {
     freeroomsData[buildingId][roomId] = {};
   }
+
   if (!(roomName in freeroomsData[buildingId][roomId])) {
     freeroomsData[buildingId][roomId]["name"] = roomName;
   }
+
   if (!(currentWeek in freeroomsData[buildingId][roomId])) {
     freeroomsData[buildingId][roomId][currentWeek] = {};
   }
+
   if (!(day in freeroomsData[buildingId][roomId][currentWeek])) {
     freeroomsData[buildingId][roomId][currentWeek][day] = [];
   }
+
   freeroomsData[buildingId][roomId][currentWeek][day].push({
     courseCode: courseName,
-    start: start,
-    end: end,
+    start: startTime,
+    end: endTime,
   });
 }
 
