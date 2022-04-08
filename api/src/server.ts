04f221441e2a0c1fcf5a86028f8c170a4f416892
OAK-02 <omarbdllh1@gmail.com>
2022-04-08 14:35:45 +1000

    adding sentry set-up

--- api/src/server.ts
+++ api/src/server.ts
@@ -3,8 +3,25 @@ import cors from "cors";
 import * as notangles from "./notangles/index";
 import * as freerooms from "./freerooms/index";
 import { writeData } from "./write-data";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 const app = express();
+
+Sentry.init({
+    dsn: "https://d053cb7b3637458e974012782ee5f662@o1179870.ingest.sentry.io/6311064",
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app }),
+    ],
+    // might need to change this for production
+    tracesSampleRate: 0.5,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
+
 const port = process.env.PORT || 3001;
 
 app.use(cors());
@@ -14,6 +31,13 @@ app.get("/api/terms/:termId/courses/:courseId", notangles.getCourse);
 app.get("/api/terms/:termId/courses", notangles.getCourseList);
 app.get("/api/terms/:termId/freerooms", freerooms.getFreeroomsData);
 app.post("/internal/scrape", writeData);
+
+
+app.use(Sentry.Handlers.errorHandler());
+app.use(function onError(err, req, res, next) {
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+});
 
 app.listen(port, () => {
     console.log(`App is running at http://localhost:${port}.`);
