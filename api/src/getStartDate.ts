af2911a36789ec7771b9b310bc22d15c2bc11e1e
Ray <raiyan.ahmed@gmail.com>
2022-05-27 16:39:03 +1000

    Fixed most of MJ's comments on fixing code quality

--- api/src/getStartDate.ts
+++ api/src/getStartDate.ts
@@ -1,28 +1,29 @@
 import * as express from "express";
 import { data } from "./load-data";
-const FIRST_COURSE: Number = 0;
+const FIRST_COURSE = 0;
+const DATA_NOT_FOUND = "ERROR";
 const termArray: string[] = ["Summer", "T1", "T2", "T3"];
 
 /**
  * Some notes on the start date for the automation:
- * To start of there are some assumptions which need to be documented:
- * 1) The course that is selected as the first class of the term is the one
- * that is used to get the term date, after
- * some sanity checks, it was concluded that it works!
+ * To start off, there are some assumptions which need to be documented:
  *
- * 2) ECON1101 is the class of interest for which we check if a
- *    particular term data is out or not This is because it
- *    is offered every term. If this changes later, please
- *    do change this code.
+ * 1)   The course that is selected as the first class of the term is the one
+ *      that is used to get the term date, after
+ *      some sanity checks, it was concluded that it works!
+ *
+ * 2)   ECON1101 is the class of interest for which we check if a
+ *      particular term data is out or not. This is because it
+ *      is offered every term. If this changes later, please
+ *      do change this code.
  *
  *
  * -> There are two different start dates for the automation.
  *    One for Notangles and the other for Freerooms.
  *    Notangles will get the most recent term start date.
- *    Freerooms will get the start date of the new term
- *    only if the current date is after the start date of the term.
- *    this will mean that freerooms would not break in the older
- *    term relative to the term the new data has been released for.
+ *    Freerooms will get the start date of the new term only after the date is
+ *    equal to the current date.
+ *
  */
 
 /**
@@ -34,43 +35,35 @@ const getStartDateFreerooms = (req: express.Request, res: express.Response) => {
         // Get the latest term and check the first class of the term
         // The assumption is that the starting date is the start date of the first class
         // of the term.
-        const currTerm = term(); // Get the current term that has the most recent data.
+        const mostRecentTerm = getLatestTermName();
         const termStartDate =
-            data.timetableData[currTerm][FIRST_COURSE]["classes"][0]["termDates"]["start"];
-        const currDate = new Date(); // We will check if the current date is after the
-        // term start date so Freerooms does not suffer.
-        // Regex expression to match the timeDelta format.
-        let regexp = /(\d{2})\/(\d{2})\/(\d{4})/;
+            data.timetableData[mostRecentTerm][FIRST_COURSE]["classes"][0]["termDates"]["start"];
+        if (termStartDate === DATA_NOT_FOUND) {
+            res.status(400).send("Error");
+        }
+
+        const currDate = new Date();
+        const regexp = /(\d{2})\/(\d{2})\/(\d{4})/;
         let day, month, year;
-        let matched = termStartDate.match(regexp);
+        const matched = termStartDate.match(regexp);
         if (matched != null) {
-            year = matched[3];
-            month = matched[2];
-            day = matched[1];
+            [, day, month, year] = matched;
         } else {
-            res.send("Error getting the start date");
+            res.status(400).send("Error getting the start date");
         }
         const termStartDateObj = new Date(year, month, day);
         // Getting the difference in time so Freerooms gets the most recent term start date when
         // it starts.
-        const timeDelta = currDate.valueOf() - termStartDateObj.valueOf();
-
-        if (timeDelta >= 0) {
-            // If the data is more than a day old, we will return the term start date that is for the
-            // new term, else we get the date for the old term.
+        const isCurrentTerm = currDate.valueOf() - termStartDateObj.valueOf() >= 0 ? true : false;
+        // If the data is more than a day old, we will return the term start date that is for the
+        // new term, else we get the date for the old term.
+        if (isCurrentTerm) {
             res.send(termStartDate);
         } else {
-            let dummyTerm = termArray.findIndex((term) => term == currTerm) - 1;
-            // Checking if the term is the first term, we can have case when index out of bounds.
-            if (dummyTerm < 0) {
-                dummyTerm = 0; // Sanity check to make sure the array does not break
-                // Only time this will happen is when the next year's summer starts over.
-            }
-            const oldTermStartDate =
-                data.timetableData[termArray[dummyTerm]][FIRST_COURSE]["classes"][0]["termDates"][
-                    "start"
-                ];
-            res.send(oldTermStartDate);
+            // Checking if the term is the first term, we can have case when index out of bounds
+            // as next year's summer rolls over.
+            let dummyTerm = Math.max(termArray.findIndex((term) => term === mostRecentTerm) - 1, 0);
+            res.send(getTermStartDate(termArray[dummyTerm]));
         }
     } catch (e) {
         res.status(400).send("Error");
@@ -85,8 +78,10 @@ const getStartDateNotangles = (req: express.Request, res: express.Response) => {
         // Get the latest term and check the first class of the term
         // The assumption is that the starting date is the start date of the first class
         // of the term.
-        const termStartDate =
-            data.timetableData[term()][FIRST_COURSE]["classes"][0]["termDates"]["start"];
+        const termStartDate = getTermStartDate(getLatestTermName());
+        if (termStartDate === DATA_NOT_FOUND) {
+            res.status(400).send("Error");
+        }
         res.send(termStartDate);
     } catch (e) {
         res.status(400).send("Error");
@@ -98,7 +93,7 @@ const getStartDateNotangles = (req: express.Request, res: express.Response) => {
  */
 const getAvailableTermData = (req: express.Request, res: express.Response) => {
     try {
-        res.send(term());
+        res.send(getLatestTermName());
     } catch (e) {
         res.status(400).send("Error");
     }
@@ -106,19 +101,19 @@ const getAvailableTermData = (req: express.Request, res: express.Response) => {
 
 /**
  * Get the term which has data from data.json, this term
- * is the latest term with data. This is exclusively for Notangles, so
- * Freerooms please do not use it!
- * @returns the latest term number
+ * is the latest term with data. This is exclusively for finding
+ * the latest term.
+ * @returns the latest term name
  */
-const term = () => {
+const getLatestTermName = () => {
     try {
         let term: string = "";
         for (let termId of termArray) {
-            let timetableData = data.timetableData;
+            const timetableData = data.timetableData;
             // Check if the termId exists in the timetableData and the fields are actually
             // present. If they are, return the term as it is.
             if (timetableData.hasOwnProperty(termId)) {
-                if (!isClassFound(termId, "ECON1101")) {
+                if (!isClassFound(termId, "ECO")) {
                     break;
                 }
                 term = termId;
@@ -127,18 +122,28 @@ const term = () => {
 
         return term;
     } catch (e) {
-        console.log("There was an error getting the most updated term!");
+        console.error("There was an error getting the most updated term!");
+        return DATA_NOT_FOUND;
     }
 };
 
 const isClassFound = (termId: string, courseCode: string) => {
-    let timetableData = data.timetableData;
-    for (let classData of timetableData[termId]) {
-        if (classData.courseCode == courseCode) {
-            return true;
-        }
+    const timetableData = data.timetableData;
+    return timetableData[termId].some((course) => course.courseCode === courseCode);
+};
+
+const getTermStartDate = (termId: string) => {
+    try {
+        // Get the latest term and check the first class of the term
+        // The assumption is that the starting date is the start date of the first class
+        // of the term.
+        const termStartDate =
+            data.timetableData[termId][FIRST_COURSE]["classes"][0]["termDates"]["start"];
+        return termStartDate;
+    } catch (e) {
+        console.error("There was an error getting the term start date!");
+        return DATA_NOT_FOUND;
     }
-    return false;
 };
 
 export { getStartDateNotangles, getStartDateFreerooms, getAvailableTermData };
