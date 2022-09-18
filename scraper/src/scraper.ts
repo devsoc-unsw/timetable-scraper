0b3afc7e0da485f1a574182c131e7d9cec2ae7df
Mun Joon Teo <teomunjoon101@gmail.com>
2022-09-18 15:07:18 +1000

    fix(scraper): Changed == to ===

--- scraper/src/scraper.ts
+++ scraper/src/scraper.ts
@@ -183,7 +183,7 @@ const timetableScraper = async (
               const termlist: ExtendedTerm[] = getTermFromCourse({
                 course: scrapedCourse,
               });
-              if (termlist == []) {
+              if (termlist === []) {
                 termlist.push(OtherTerms.Other);
               }
               const { notes } = scrapedCourse;
